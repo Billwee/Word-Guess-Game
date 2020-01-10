4fd8cba6909d31a8222bde9a06043f188bc4e625
Bill Kamberos <bill.kamberos@gmail.com>
2020-01-09 22:13:39 -0500

    Html test skeleton done - a few functions added to .js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,4 @@
-// Welcome to my Psychic Game. Not only did I create
-// the game for the assignment.. I also added a function
-// that only allows letter keys to be pressed and an
-// if statement that doesn't allow you to press the
-// same key twice. Enjoy!
-
-var alphabet = 'abcdefghijklmnopqrstuvwxyz';
+var alphabet = 'abcdefghijklmnopqrstuvwxyz ';
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
@@ -14,93 +8,106 @@ var winsHTML = document.getElementById('wins');
 var loseHTML = document.getElementById('losses');
 var guessesLeftHTML = document.getElementById('guessesLeft');
 var guessedHTML = document.getElementById('guessed');
+let dash = [];
 
-// Function that assigns a random letter to randomLetter
-function letter() {
-  randomLetter = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
-}
+let game = {
+  shows: ['but ts', 'wei ners', 'boo gers', 'ding dongs'],
 
-// Function that will return true if a letter is pressed.
-// I found the "key.toLowerCase() != key.toUpperCase()"
-// part on StackOverflow but added 'key.length === 1'
-// to also rule out the arrow keys and the 6 keys above them.
-function isLetter(key) {
-  if (key.toLowerCase() != key.toUpperCase() && key.length === 1) {
-    return true;
-  } else {
-    return false;
+  //Checks is input is a letter
+  isLetter: function(key) {
+    if (!(key.toLowerCase() != key.toUpperCase() && key.length === 1)) {
+      return alert('Press a letter');
+    }
+  },
+
+  isGuessed: function(key) {
+    if (alphabet.indexOf(key) >= 0) {
+      alphabet = alphabet.replace(key, '');
+      // console.log(alphabet);
+    } else {
+      alert(`You already pressed ${key}`);
+      guessesLeft += 1;
+      yourGuesses.pop();
+    }
+  },
+
+  getshow: function() {
+    randomShow = this.shows[Math.floor(Math.random() * this.shows.length)];
+  },
+
+  dashArray: function(show) {
+    if (dash.length === 0) {
+      for (let i = 0; i < show.length; i++) {
+        dash.push('_');
+      }
+    }
+  },
+
+  spaces: function() {
+    for (let i = 0; i < randomShow.length; i++) {
+      if (randomShow[i] == ' ') {
+        dash[i] = randomShow[i];
+      }
+    }
+    console.log(dash);
+  },
+
+  reveal: function(key, show) {
+    for (let i = 0; i < show.length; i++) {
+      if (key === randomShow[i]) {
+        dash[i] = randomShow.charAt(i);
+      }
+    }
+    console.log(dash);
   }
-}
+};
 
-// Displaying inital values on webpage
+game.getshow();
+
+console.log(randomShow);
+
 winsHTML.innerHTML = wins;
-loseHTML.innerHTML = losses;
 guessesLeftHTML.innerHTML = guessesLeft;
-
-// Setting first random letter
-letter();
-
-// I added this to the beginning and every game reset
-// so you can see what the random letter is.
-console.log(randomLetter);
 
 // Everything is in this onkeyup function
 document.onkeyup = function(event) {
   keyPress = event.key;
-  // Checking if what you pressed is a letter
-  if (!isLetter(keyPress)) {
-    return alert('Enter a letter');
-  }
-  // Checking if the key you pressed was already guessed.
-  // After writing this I realized I could have used the
-  // yourGuesses array as a refernce point instead of a
-  // string that your replacing the guesses in with empty spaces.
-  // The guesses were still appearing in the array despite being a
-  // duplicate and the guesses counter was going down so I
-  // adjusted them. Also logged the alphabet string after every
-  // guess so you can see it in action.
-  if (alphabet.indexOf(keyPress) >= 0) {
-    alphabet = alphabet.replace(keyPress, '');
-    console.log(alphabet);
-  } else {
-    alert(`You already pressed ${keyPress}`);
-    guessesLeft += 1;
-    yourGuesses.pop();
-  }
-  // The game itself. When guessing, wrong guess lowers guessesLeft
-  // and adds it to the displayed array. A right guess or 9 wrong
-  // guesses adds 1 to wins or losses and resets the guessesLeft,
-  // yourGuesses array, and the alphabet string to start a new game.
-  if (guessesLeft >= 2) {
-    if (keyPress.toLowerCase() !== randomLetter) {
-      guessesLeft -= 1;
-      yourGuesses.push(' ' + keyPress.toLowerCase() + ' ');
-      guessedHTML.innerHTML = yourGuesses;
-      guessesLeftHTML.innerHTML = guessesLeft;
-    } else {
-      return (
-        (wins += 1),
-        (guessesLeft = 9),
-        (guessesLeftHTML.innerHTML = guessesLeft),
-        (winsHTML.innerHTML = wins),
-        (yourGuesses = []),
-        (guessedHTML.innerHTML = yourGuesses),
-        letter(),
-        console.log(randomLetter),
-        (alphabet = 'abcdefghijklmnopqrstuvwxyz')
-      );
-    }
-  } else {
-    return (
-      (losses += 1),
-      (guessesLeft = 9),
-      (guessesLeftHTML.innerHTML = guessesLeft),
-      (loseHTML.innerHTML = losses),
-      (yourGuesses = []),
-      (guessedHTML.innerHTML = yourGuesses),
-      letter(),
-      console.log(randomLetter),
-      (alphabet = 'abcdefghijklmnopqrstuvwxyz')
-    );
-  }
+  game.isLetter(keyPress);
+  game.isGuessed(keyPress);
+  game.dashArray(randomShow);
+  game.spaces();
+  game.reveal(keyPress, randomShow);
+
+  // if (guessesLeft >= 2) {
+  //   if (keyPress.toLowerCase() !== randomShow) {
+  //     guessesLeft -= 1;
+  //     yourGuesses.push(' ' + keyPress.toLowerCase() + ' ');
+  //     guessedHTML.innerHTML = yourGuesses;
+  //     guessesLeftHTML.innerHTML = guessesLeft;
+  //   } else {
+  //     return (
+  //       (wins += 1),
+  //       (guessesLeft = 9),
+  //       (guessesLeftHTML.innerHTML = guessesLeft),
+  //       (winsHTML.innerHTML = wins),
+  //       (yourGuesses = []),
+  //       (guessedHTML.innerHTML = yourGuesses),
+  //       letter(),
+  //       console.log(randomShow),
+  //       (alphabet = 'abcdefghijklmnopqrstuvwxyz')
+  //     );
+  //   }
+  // } else {
+  //   return (
+  //     (losses += 1),
+  //     (guessesLeft = 9),
+  //     (guessesLeftHTML.innerHTML = guessesLeft),
+  //     (loseHTML.innerHTML = losses),
+  //     (yourGuesses = []),
+  //     (guessedHTML.innerHTML = yourGuesses),
+  //     letter(),
+  //     console.log(randomShow),
+  //     (alphabet = 'abcdefghijklmnopqrstuvwxyz')
+  //   );
+  // }
 };
